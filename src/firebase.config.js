580f9275c98bef034cb798310a187e3367872913
Philip <philipharteveld3110@gmail.com>
2021-12-26 12:45:46 +0200

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCJzB204-0A46j4pQnzD1UycqAvDi6DVWM",
+  authDomain: "house-market-app-6525f.firebaseapp.com",
+  projectId: "house-market-app-6525f",
+  storageBucket: "house-market-app-6525f.appspot.com",
+  messagingSenderId: "622465182268",
+  appId: "1:622465182268:web:1030865c92cc2444893139",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
