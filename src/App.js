1ed122ca4c7d0c0b4a35e568f6e0b047ed81de82
Philip <philipharteveld3110@gmail.com>
2021-12-27 10:09:43 +0200

    Update user sign In and Sign Up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <>
-      <h1>My app</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgotpassword" element={<ForgotPassword />} />
+        </Routes>
+        <NavBar />
+      </Router>
+      <ToastContainer />
     </>
   );
 }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,71 @@
+import { useNavigate, useLocation } from "react-router-dom";
+import { ReactComponent as OfferIcon } from "../assests/svg/localOfferIcon.svg";
+import { ReactComponent as ExploreIcon } from "../assests/svg/exploreIcon.svg";
+import { ReactComponent as PersonOutlineIcon } from "../assests/svg/personOutlineIcon.svg";
+function NavBar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathMatchRoute = (route) => {
+    if (route === location.pathname) {
+      return true;
+    }
+  };
+  return (
+    <footer className="navbar">
+      <div className="navbarNav">
+        <ul className="navbarListItems">
+          <li className="navbarListItem" onClick={() => navigate("/")}>
+            <ExploreIcon
+              fill={pathMatchRoute("/") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Explore
+            </p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate("/offers")}>
+            <OfferIcon
+              fill={pathMatchRoute("/offers") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/offers")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Offer
+            </p>
+          </li>
+          <li className="navbarListItem" onClick={() => navigate("/profile")}>
+            <PersonOutlineIcon
+              fill={pathMatchRoute("/profile") ? "#2c2c2c" : "#8f8f8f"}
+              width="36px"
+              height="36px"
+            />
+            <p
+              className={
+                pathMatchRoute("/profile")
+                  ? "navbarListItemNameActive"
+                  : "navbarListItemName"
+              }
+            >
+              Profile
+            </p>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default NavBar;
